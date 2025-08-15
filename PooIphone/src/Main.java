5360b1b2fbd7b9d5cf0b72010c9655c4ba0310c1
Dinarte Filho <dinarte_filho@hotmail.com>
2025-08-14 23:25:29 -0300

    feat: adding functionality calls in main

--- PooIphone/src/Main.java
+++ PooIphone/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        Iphone iphone = new Iphone();
 
+        iphone.selecionarMusica("Grandes coisas - Fernandinho");
+        iphone.tocar();
+        iphone.pausar();
+
+        iphone.ligar("(4002-8922)");
+        iphone.iniciarCorreioVoz();
+        iphone.encerrarChamada();
+
+        iphone.exibirPagina("https://github.com/dinarteefilho");
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
     }
 }
